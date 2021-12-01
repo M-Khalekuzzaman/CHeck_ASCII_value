b75a2462ba33cebd21e263197f7d3fc19a7e2c25
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-12-01 16:39:59 +0600

    Add files via upload

---
+++ Check ASCII value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int n;
+    printf("Enter your ASCII value is : ");
+    scanf("%d",&n);
+
+    printf("Ascii character is :%c\n ",n);
+
+    char c;
+    printf("Enter your character is : ");
+    scanf("%c",&c);
+
+    printf("The ASCII number value is : %d\n",c);
+    getch();
+}
